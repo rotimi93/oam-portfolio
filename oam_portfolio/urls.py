7d5bac734819bad2d8bec3ea35013e921cae3191
Micheal rotimi <60150001+rotimi93@users.noreply.github.com>
2022-05-27 08:18:44 +0100

    Update urls.py

--- oam_portfolio/urls.py
+++ oam_portfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from oam_portfolio.oamViews import indexView, detailView, workshopView
 
 urlpatterns = [
-    path('home/', indexView.home, name='cv-view'),
+    path('', indexView.home, name='cv-view'),
     path('Detail/', detailView.detail, name='detail-view'),
     path('workshop/', workshopView.ws, name='ws-view')
 ]
