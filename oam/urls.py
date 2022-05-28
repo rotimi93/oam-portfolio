e0aad3e803a2cf276d46c96614ba526e41072588
Micheal rotimi <60150001+rotimi93@users.noreply.github.com>
2022-05-28 14:32:41 +0100

    Update urls.py

--- oam/urls.py
+++ oam/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('oamportfolio/', include('oam_portfolio.urls')),
+    path('/', include('oam_portfolio.urls')),
     path('admin/', admin.site.urls),
 ]
